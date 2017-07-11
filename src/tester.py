739cb3fa853104d6b023e532541d7db46716f9f0
mathnerdrk <rahul.krishnan@hotmail.com>
2017-07-11 14:33:53 -0600

    Added tester.py: GUI w/mouse testing

---
+++ src/tester.py
@@ -0,0 +1,193 @@
+# %load network.py
+
+"""
+Modified network.py
+"""
+
+#### Libraries
+# Standard library
+import random
+
+# Third-party libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Network(object):
+
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+            test_data=None):
+
+        training_data = list(training_data)
+        n = len(training_data)
+
+        if test_data:
+            test_data = list(test_data)
+            n_test = len(test_data)
+
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test));
+            else:
+                print("Epoch {} complete".format(j))
+
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x] 
+        zs = [] 
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_derivative(activations[-1], y) * \
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+    
+    def test(self, img):
+        saveImg = cv2.resize(img, (28, 28))
+        gray = rgb2gray(saveImg)
+        gray2 = inverte(gray)
+        plt.imshow(gray, cmap = plt.get_cmap('gray'))   
+        imgInput = np.reshape(gray2, (784, 1))
+        result = np.argmax(self.feedforward(imgInput))
+        return result
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations-y)
+
+#### Miscellaneous functions
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    return sigmoid(z)*(1-sigmoid(z))
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+def inverte(imagem):
+    return (255-imagem)
+
+
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+tester.py
+Created on Fri Jul  7 14:04:25 2017
+
+@author: Rahul Krishnan
+"""
+
+#modified from http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_mouse_handling/py_mouse_handling.html
+
+import cv2
+import tkinter.messagebox
+
+drawing = False # true if mouse is pressed
+mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+ix,iy = -1,-1
+
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    global ix,iy,drawing,mode
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.circle(img,(x,y),10,(0,0,0),-1)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.circle(img,(x,y),10,(0,0,0),-1)
+
+def clearBoard(img):
+    cv2.rectangle(img, (0,0), (255, 255), (255, 255, 255), -1)
+        
+def saveImg(img):
+    saveImg = cv2.resize(img, (28, 28)) 
+    cv2.imwrite('01.png',saveImg)
+    clearBoard(img)
+    
+def getImg(img, net):
+    result = net.test(img)
+    tkinter.messagebox.showinfo("info", 'You outputted a {}'.format(result))
+    clearBoard(img)
+
+
+"""Run Network to Train Data"""
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+
+"""Draw Screen"""            
+img = np.ones((255,255,3), np.uint8)*255
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_circle)
+
+while(1):
+    cv2.imshow('image',img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k == 27:
+        break
+    elif k == ord('c'):
+        clearBoard(img)
+    elif k == ord('s'):
+        saveImg(img)
+    elif k == ord('t'):
+        getImg(img, net)
+    elif k == ord('i'):
+        imgNew = cv2.imread('01.png')
+        getImg(imgNew, net)
+
+cv2.destroyAllWindows()
